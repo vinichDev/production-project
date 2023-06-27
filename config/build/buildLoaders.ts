a092fbd5331bdc4b1a4350a81fa10bffbe87a013
Alexander Vinichenko <vinich.yota@gmail.com>
2023-06-27 18:55:12 +0300

    initial commit

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,38 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+
+    const {isDev} = options;
+
+    const cssLoader: webpack.RuleSetRule = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: /\.module\./,
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
+                    },
+                }
+            },
+            "sass-loader",
+        ],
+    };
+
+    const typescriptLoader: webpack.RuleSetRule = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    };
+
+    return [
+        cssLoader,
+        typescriptLoader,
+    ]
+}
---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './styles/index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Suspense, useContext} from "react";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
+
+const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>Toggle</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
